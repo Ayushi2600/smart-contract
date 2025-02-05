f8bda7c21d0024bd280f6607fda93bf44f5eeb0a
Ayushi Dhiman <ayushidhiman666@gmail.com>
2025-02-05 13:49:47 +0530

    Adding comments

--- programs/create_and_use_pda/src/lib.rs
+++ programs/create_and_use_pda/src/lib.rs
@@ -1,17 +1,24 @@
+// This contract allows users to create their own counter stored in a PDA and also then increment their counter.
+
+// Import anchor 
 use anchor_lang::prelude::*;
 
-declare_id!("5Hsvd5MQfukMrBJY28Q2YouAfYRxM5ZyKPj3toQqsqS1");
+// This is a unique address that helps clients to identify and interact with the program. 
+declare_id!("5Hsvd5MQfukMrBJY28Q2YouAfYRxM5ZyKPj3toQqsqS1"); 
 
+// Define the program
 #[program]
 pub mod create_and_use_pda {
     use super::*;
 
+    // This function creates a PDA account for the user and counter start at 0.
     pub fn create_counter(ctx: Context<CreateCounter>) -> Result<()> {
         let base_account = &mut ctx.accounts.base_account;
         base_account.count = 0;
         Ok(())
     }
 
+    // This function retrieve the user's PDA and increment the counter.
     pub fn increment_counter(ctx: Context<IncrementCounter>) -> Result<()> {
         let base_account = &mut ctx.accounts.base_account;
         base_account.count += 1;
@@ -19,14 +26,15 @@ pub mod create_and_use_pda {
     }
 }
 
+// Account structure
 #[derive(Accounts)]
 pub struct CreateCounter<'info> {
-    // "counter" is a static seed and user.key().as_ref() ensures that each user gets their own counter
+
     #[account(
         init, 
         payer = user, 
         space = 8 + 8,
-        seeds = [b"counter", user.key().as_ref()], // PDA serived from user wallet
+        seeds = [b"counter", user.key().as_ref()], // PDA serived from user wallet, "counter" is a static seed and user.key().as_ref() ensures that each user gets their own counter
         bump,
     )]
     pub base_account : Account<'info, BaseAccount>,
@@ -40,7 +48,7 @@ pub struct CreateCounter<'info> {
 pub struct IncrementCounter<'info> {
     #[account(
         mut,
-        seeds = [b"counter", user.key().as_ref()], // same seed as used in CreateCounter
+        seeds = [b"counter", user.key().as_ref()], // find the existing PDA using the same seed as used in CreateCounter
         bump,
     )]
     pub base_account : Account<'info, BaseAccount>,
@@ -48,7 +56,7 @@ pub struct IncrementCounter<'info> {
     #[account(mut)]
     pub user : Signer<'info>,
 }
-
+// BaseAccount structure
 #[account]
 pub struct BaseAccount {
     pub count : u64,
